0debf959978fd3b5df3cf4553c518dee663728cf
Ghulam Mujtaba <ghulam.mujtaba.dogar@gmail.com>
2020-07-15 16:36:02 +0500

    Initial

---
+++ Assets/Scenes/Game menu/scripts/GameMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenuController : MonoBehaviour
+{
+    public void onclickpenguin()
+    {
+        SceneManager.LoadScene("Penguins");
+    }
+    public void onclickhummingbird()
+    {
+        SceneManager.LoadScene("FlowerIsland");
+    }
+}
+
